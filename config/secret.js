651fa6bd5025440167b88ead61c9ea1250b02a7b
skolly <skolly150@gmail.com>
2020-12-07 09:45:08 +0100

    correction of server port

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
     database : 'mongodb+srv://greenlabMicrofactory:JESUloluwa1@cluster0.cvwp6.mongodb.net/greenlab?retryWrites=true&w=majority',
-    port: 9000 
+    port: 3000 
     }
     
